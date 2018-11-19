f5cdea1b5a60046cf341d28cc534c7b3bbcbc3e5
Austin Rovge <rovgea@msoe.edu>
2018-11-19 16:27:21 -0600

    added comments and method for starting the http server

--- server.py
+++ server.py
@@ -1,5 +1,24 @@
+"""
+Python Webserver
+ - 11/19/2018
+ - Austin Rovge
+
+This is a simple web server written in python.
+"""
+
+
 def main():
-    print('test')
+    """
+    The main method is called first and starts the server on port 8080.
+    """
+    start_http_server(8080)
+
+
+def start_http_server(port):
+    """
+    This method setups the http server
+    :param port: the port to start the server on
+    """
 
 
 if __name__ == '__main__':
