624d6f8cc04710a5a6fe54fd6dd8ed7219a65e0b
Austin Rovge <rovgea@msoe.edu>
2018-11-19 16:32:04 -0600

    server creates a socket and will begin listening for connections. prints a connection when it is received

--- server.py
+++ server.py
@@ -6,11 +6,14 @@ Python Webserver
 This is a simple web server written in python.
 """
 
+import socket
+
 
 def main():
     """
     The main method is called first and starts the server on port 8080.
     """
+
     start_http_server(8080)
 
 
@@ -19,6 +22,16 @@ def start_http_server(port):
     This method setups the http server
     :param port: the port to start the server on
     """
+
+    server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+    listen_addr = '', port
+    server_socket.bind(listen_addr)
+    server_socket.listen()
+
+    while True:
+        conn, addr = server_socket.accept()
+        print(conn)
 
 
 if __name__ == '__main__':
